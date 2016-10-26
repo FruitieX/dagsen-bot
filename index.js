c0f63b420440e8a102c5385919b88e2478e6986e
Jonas Berg <jonas.berg@relex.fi>
2016-10-26 17:15:22 +0300

    Added Wappen functionality.

--- index.js
+++ index.js
@@ -126,6 +126,14 @@ tg.on('message', function(msg) {
       text: 'Spela trivia nu: https://telegram.me/joinchat/AXjh-gBgic0Dnbj_-uzMxg',
       chat_id: msg.chat.id
     });
+  } else if (!msg.text.indexOf('/wappen')) {
+    var today = new Date();
+    var wappen = new Date(today.getMonth()>=4 ? today.getFullYear()+1 : today.getFullYear(),4,30); // Months start from 0.
+    var daysLeft = Math.round((wappen-today)/(1000*60*60*24));
+    tg.sendMessage({
+      text: 'Det är ' + daysLeft + ' dagar kvar till wappen (om den ordnas)!',
+      chat_id: msg.chat.id
+    });
   }
 });
 
