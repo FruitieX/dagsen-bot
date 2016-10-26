66a953cbde7b50d410b35f5c9240bde01fa18371
Oleg Stikhin <oleg.stikhin@gmail.com>
2016-10-26 19:28:22 +0300

    Bompa (#17)
    * updating telegram-bot
    
    * add Bompa

--- index.js
+++ index.js
@@ -121,6 +121,11 @@ tg.on('message', function(msg) {
       text: 'Spela trivia nu: https://telegram.me/joinchat/AXjh-gBgic0Dnbj_-uzMxg',
       chat_id: msg.chat.id
     });
+  } else if (!msg.text.indexOf('/bompa')) {
+    tg.sendAudio({
+      audio: 'BQADBAADNgAD-uF4AZWtSWQU4TIFAg',
+      chat_id: msg.chat.id
+    });
   } else if (!msg.text.indexOf('/wappen')) {
     var today = new Date();
     var wappen = new Date(today.getMonth()>=4 ? today.getFullYear()+1 : today.getFullYear(),4,30); // Months start from 0.
